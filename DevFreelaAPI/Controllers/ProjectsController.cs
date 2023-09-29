34fe787cef1587455ef9076ae9d6adebaf4ad2a8
Murilo Nascimento <murilohnascimento90murilohnascimento90@outlook.com.br>
2023-09-28 21:01:48 -0300

    Aula - Controllers e Actions. Adicionando ProjectsController.

---
+++ DevFreelaAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreelaAPI.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController: Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
