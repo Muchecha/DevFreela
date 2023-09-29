9361455359d29c418f423261bd782d6f92969065
Murilo Nascimento <murilohnascimento90murilohnascimento90@outlook.com.br>
2023-09-28 22:11:21 -0300

    Nessa branch criarmos CreateProjectModel e UpdateProjectModel.

--- DevFreelaAPI/Controllers/ProjectsController.cs
+++ DevFreelaAPI/Controllers/ProjectsController.cs
@@ -1,14 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreelaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreelaAPI.Controllers
 {
     [Route("api/projects")]
     public class ProjectsController: Controller
     {
+        // api/projects?query=net core
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
             return Ok();
+        }
+
+        // api/projects/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // return NotFound();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProjectModel)
+        {
+            if(createProjectModel.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(GetById), new { id = createProjectModel.Id },
+                                                                    createProjectModel);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpadateProjectModel upadateProjectModel)
+        {
+            if(upadateProjectModel.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        // api/projects/3 DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
         }
     }
 }
