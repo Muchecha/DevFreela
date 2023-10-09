ac2531aae4c8cd920ea4aa62f7992e68be67b646
Murilo Nascimento <murilohnascimento90murilohnascimento90@outlook.com.br>
2023-10-09 19:33:37 -0300

    Camada Application parte 5

--- DevFreela.Application/ViewModels/ProjectViewModel.cs
+++ DevFreela.Application/ViewModels/ProjectViewModel.cs
@@ -2,12 +2,14 @@
 {
     public class ProjectViewModel
     {
-        public ProjectViewModel(string title, DateTime creadeAt)
+        public ProjectViewModel(int id, string title, DateTime creadeAt)
         {
+            Id = id;
             Title = title;
             CreadeAt = creadeAt;
         }
 
+        public int Id { get; private set; }
         public string Title { get; private set; }
         public DateTime CreadeAt { get; private set; }
     }
