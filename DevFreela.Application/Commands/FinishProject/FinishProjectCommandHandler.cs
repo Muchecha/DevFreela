33efc67017fa55fe09aedd58e3cffdbceac4676a
Murilo Nascimento <murilohnascimento90murilohnascimento90@outlook.com.br>
2023-10-19 20:28:40 -0300

    Update no projeto Application /Commands

--- DevFreela.Application/Commands/FinishProject/FinishProjectCommandHandler.cs
+++ DevFreela.Application/Commands/FinishProject/FinishProjectCommandHandler.cs
@@ -1,22 +1,22 @@
-﻿using DevFreela.Infrastructure.Persistence;
-using MediatR;
+﻿using MediatR;
 
 namespace DevFreela.Application.Commands.FinishProject
 {
-    public class FinishProjectCommandHandler: IRequestHandler<FinishProjectCommand, Unit>
+    public class FinishProjectCommandHandler : IRequestHandler<FinishProjectCommand, Unit>
     {
-        private readonly DevFreelaDbContext _dbContext;
-        public FinishProjectCommandHandler(DevFreelaDbContext dbContext)
+        private readonly IProjectRepository _projectRepository;
+        public FinishProjectCommandHandler(IProjectRepository projectRepository)
         {
-            _dbContext = dbContext;
+            _projectRepository = projectRepository;
         }
+
         public async Task<Unit> Handle(FinishProjectCommand request, CancellationToken cancellationToken)
         {
-            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == request.Id);
+            var project = await _projectRepository.GetByIdAsync(request.Id);
 
             project.Finish();
 
-            await _dbContext.SaveChangesAsync();
+            await _projectRepository.SaveChangesAsync();
 
             return Unit.Value;
         }
