08543248d0077c0cf14ad92c23048a71e0903435
Murilo Nascimento <murilohnascimento90murilohnascimento90@outlook.com.br>
2023-10-19 20:37:26 -0300

    Update projeto Core / Exceptions

--- DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
+++ DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
@@ -1,8 +1,8 @@
 ﻿namespace DevFreela.Core.Exceptions
 {
-    public class ProjectAlreadyStartedException : Exception
+    public class ProjectAlreadyStartedException: Exception
     {
-        public ProjectAlreadyStartedException() : base("Project is already in Stated status")
+        public ProjectAlreadyStartedException() : base("Project is already in Started status")
         {
         }
     }
